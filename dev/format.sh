901ed0a71de641e832e3f96a5bdf329e282f4c81
Philip Caplan <caplan.philip@gmail.com>
2023-12-29 14:32:22 -0500

    add scripts to setup env and format

---
+++ dev/format.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+clang-format --style=Google -i src/*.cpp src/*.h src/*.hpp src/math/*.cpp src/math/*.h src/math/*.hpp
---
+++ dev/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        # Linux
+	sudo apt-get update -qq
+        sudo apt-get install -y --no-install-recommends libgl1-mesa-dev	
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+        # Mac OSX
+	xcode-select --install
+	brew install clang-format
+else
+	echo "unsupported operating system $OSTYPE"
+fi
