e3684ae93aaa2d47cf40c44a6572e08470e0f1fe
Philip Caplan <caplan.philip@gmail.com>
2024-01-08 07:40:53 -0500

    reduce running time of examples

--- dev/examples.sh
+++ dev/examples.sh
@@ -4,13 +4,12 @@ vortex=$1
 
 # create adapted triangle mesh of oceans and continents
 if [ ! -f data/earth.meshb ]; then
-  $vortex mesh data/oceans_2048.png --output data/earth.meshb
+  # pass 0.02 in workflow, but 0.005 for better resolution
+  $vortex mesh data/oceans_2048.png --hmin $2 --output data/earth.meshb
 fi
-$vortex mesh data/oceans_2048.png --hmin 0.02 --output data/earth_coarse.meshb
 
 # extract oceans and land
 $vortex extract data/earth.meshb --oceans data/oceans.meshb --continents data/land.meshb
-$vortex extract data/earth_coarse.meshb --oceans data/oceans_coarse.meshb --continents data/land_coarse.meshb
 
 # voronoi diagrams
 $vortex voronoi --domain sphere --points random --n_points 100000 --n_smooth 10 --output data/voronoi_sphere.meshb
