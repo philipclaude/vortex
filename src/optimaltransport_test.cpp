6ad8682f0b0bc2f8b62e445cb7086488cec9e9c2
Hokalaka <otis.true@gmail.com>
2024-03-04 14:18:41 -0500

    problem with nlopt

--- src/optimaltransport_test.cpp
+++ src/optimaltransport_test.cpp
@@ -24,6 +24,7 @@
 #include "graphics.h"
 #include "io.h"
 #include "library.h"
+#include <nlopt.hpp>
 #include "math/vec.hpp"
 #include "tester.h"
 #include "util.h"
@@ -34,6 +35,16 @@ using namespace vortex;
 
 UT_TEST_SUITE(optimaltransport_test_suite)
 
+struct nlopt_data
+{
+    VoronoiDiagram &voronoi;
+    SphereDomain &domain;
+    Vertices &vertices;
+    VoronoiDiagramOptions &options;
+    const std::vector<double> &cell_sizes;
+    int iter;
+};
+
 double calc_triangle_area(vec3d &p0, vec3d &p1, vec3d &p2)
 {
     vec3d edge1 = p1 - p0;
@@ -43,60 +54,89 @@ double calc_triangle_area(vec3d &p0, vec3d &p1, vec3d &p2)
     return 0.5 * length(crossProduct);
 };
 
-double calc_energy(VoronoiDiagram &voronoi, std::vector<double> &weights, double &idealWeight)
+double calc_energy(unsigned n, const double *x, double *de_dw, void *data0)
 {
+    nlopt_data &data = *static_cast<nlopt_data *>(data0);
+    VoronoiDiagram &voronoi = data.voronoi;
+    double totalArea = 0.0;
     const Topology<Polygon> &polygons = voronoi.polygons();
+    auto &weights = voronoi.weights();
+    weights.resize(n, 0.0);
+    for (int i = 0; i < n; i++)
+    {
+        weights[i] = x[i];
+    }
 
+    lift_sites(data.vertices, voronoi.weights());
+    data.options.store_mesh = true;
+    data.domain.set_initialization_fraction(0.7);
+    voronoi.vertices().clear();
+    voronoi.polygons().clear();
+    voronoi.triangles().clear();
+    voronoi.compute(data.domain, data.options);
+
+    data.iter++;
+
+    double error = 0.0;
     double energy = 0.0;
-    double second_sum = 0.0;
+    double sum = 0.0;
     for (int k = 0; k < polygons.n(); k++)
     {
         double cell_area = 0.0;
-        // get the site of the polygon (assumes site's within polygon)
+        // get the site of the polygon
         int group_index = polygons.group(k);
-        vec3d site(voronoi.vertices().group(group_index));
+        vec3d site(voronoi.vertices()[group_index]);
 
-        int p0_index = polygons(k, 0);
-        vec3d p0(voronoi.vertices()[p0_index]);
+        int p1_index = polygons(k, 0);
+        vec3d p1(voronoi.vertices()[p1_index]);
 
         for (int j = 1; j < voronoi.polygons().length(k) - 1; j++)
         {
             // get two vertices of the polygon
-            int p1_index = polygons(k, j);
-            int p2_index = polygons(k, j + 1);
-            vec3d p1(voronoi.vertices()[p1_index]);
+            int p2_index = polygons(k, j);
+            int p3_index = polygons(k, j + 1);
             vec3d p2(voronoi.vertices()[p2_index]);
+            vec3d p3(voronoi.vertices()[p3_index]);
 
             // calculate area of the triangle
-            double t_area = calc_triangle_area(p0, p1, p2);
+            double t_area = calc_triangle_area(p1, p2, p3);
 
             // taken directly from Levy Geogram
             double cur_f = 0.0;
             for (index_t c = 0; c < 3; c++)
             {
-                double u0 = site[c] - p0[c];
-                double u1 = site[c] - p1[c];
-                double u2 = site[c] - p2[c];
+                double u0 = site[c] - p1[c];
+                double u1 = site[c] - p2[c];
+                double u2 = site[c] - p3[c];
                 cur_f += u0 * u0;
                 cur_f += u1 * (u0 + u1);
                 cur_f += u2 * (u0 + u1 + u2);
             }
 
-            cell_area += t_area;
             energy += t_area * cur_f / 6.0;
+            cell_area += t_area;
         }
+
+        if (de_dw)
+        {
+            de_dw[k] = cell_area - data.cell_sizes[k];
+        }
+
+        sum = sum + pow((cell_area - data.cell_sizes[k]), 2);
         energy += (-weights[k] * cell_area);
-        second_sum += idealWeight * weights[k];
+        energy += data.cell_sizes[k] * weights[k];
+        totalArea += cell_area;
     }
-    return energy + second_sum;
+    LOG << fmt::format("total area = {}", totalArea);
+    error = pow((sum / n), 0.5);
+    LOG << fmt::format("iteration {}, energy = {}, error = {}", data.iter, energy, error);
+    return -energy;
 };
 
 UT_TEST_CASE(test_optimaltransport)
 {
     int n_iter = 10;
-    double delta = 10e-3;
-    double cell_size_tol = 10e-6;
-    size_t n_sites = 10000;
+    size_t n_sites = 100000;
 
     auto irand = [](int min, int max)
     {
@@ -104,8 +144,6 @@ UT_TEST_CASE(test_optimaltransport)
     };
     const double tol = 1e-12;
     static const int dim = 4; // number of dimensions
-
-    double cell_size = 4 * M_PI / n_sites;
 
     std::vector<coord_t> sites(n_sites * dim, 0.0);
 
@@ -121,18 +159,21 @@ UT_TEST_CASE(test_optimaltransport)
             sites[k * dim + 3] = 0.0;
     }
 
+    // ideal cell size
+    std::vector<double> cell_sizes(n_sites, 4 * M_PI / n_sites);
+
     std::vector<index_t> order(n_sites);
     sort_points_on_zcurve(sites.data(), n_sites, dim, order);
 
     Vertices vertices(dim);
     vertices.reserve(n_sites);
-    coord_t x[dim];
+    coord_t vertex[dim];
     // add sites in z-curve order to Vertices list
     for (size_t i = 0; i < n_sites; i++)
     {
         for (int d = 0; d < dim; d++)
-            x[d] = sites[dim * order[i] + d];
-        vertices.add(x);
+            vertex[d] = sites[dim * order[i] + d];
+        vertices.add(vertex);
     }
 
     SphereDomain domain;
@@ -141,9 +182,6 @@ UT_TEST_CASE(test_optimaltransport)
     options.n_neighbors = 75;
     options.allow_reattempt = false;
     options.parallel = true;
-
-    auto &weights = voronoi.weights();
-    weights.resize(n_sites, 0.0);
 
     for (int iter = 1; iter <= n_iter; ++iter)
     {
@@ -156,48 +194,43 @@ UT_TEST_CASE(test_optimaltransport)
 
         // move each site to the centroid of the corresponding cell
         voronoi.smooth(vertices, true);
-        auto props = voronoi.analyze();
-        LOG << fmt::format("area = {}", props.area);
-        LOG << fmt::format("iter = {}, area = {}", iter, calc_energy(voronoi, weights, cell_size));
     }
-    auto props = voronoi.analyze();
+    nlopt_data data = {voronoi, domain, vertices, options, cell_sizes, 0};
+    std::vector<double> x(n_sites, 0.0);
 
-    // // start weights as 0
-    // for (size_t k = 0; k < n_sites; k++)
+    calc_energy(n_sites, x.data(), nullptr, &data);
+    // nlopt::opt opt(nlopt::LN_NELDERMEAD, n_sites);
+
+    // opt.set_min_objective(&calc_energy, static_cast<void *>(&data));
+
+    // // set some optimization parameters
+    // opt.set_xtol_rel(1e-12);
+    // opt.set_ftol_rel(1e-6);
+    // opt.set_maxeval(10000);
+
+    // // set the lower and upper bounds on the weights
+    // std::vector<double> lower_bound(n_sites, 0.0);
+    // opt.set_lower_bounds(lower_bound);
+    // // std::vector<double> upper_bound(n_sites, 10.0);
+    // // opt.set_upper_bounds(upper_bound);
+
+    // double f_opt;
+    // try
     // {
-    //     weights[k] = 0;
+    //     auto result = opt.optimize(x, f_opt);
+    //     printf("nlopt result: %d\n", result);
+    // }
+    // catch (std::exception &e)
+    // {
+    //     std::cout << e.what() << std::endl;
+    //     printf("runtime error\n");
     // }
 
-    // // lift vertices to 4D
-    // double error;
-    // std::ofstream outFile;
-    // outFile.open("../../data_test/hundred_convergence_error.txt");
-
-    // do
-    // {
-    //     double sum = 0.0;
-    //     lift_sites(vertices, voronoi.weights());
-
-    //     domain.set_initialization_fraction(0.7);
-    //     voronoi.vertices().clear();
-    //     voronoi.polygons().clear();
-    //     voronoi.triangles().clear();
-    //     voronoi.compute(domain, options);
-
-    //     for (size_t k = 0; k < n_sites; k++)
-    //     {
-    //         weights[k] = weights[k] - delta * (voronoi.properties()[k].mass - cell_size);
-    //         sum = sum + pow((voronoi.properties()[k].mass - cell_size), 2);
-    //     }
-    //     error = pow((sum / n_sites), 0.5);
-    //     outFile << std::to_string(error) << std::endl;
-    // } while (error > cell_size_tol);
-
-    // outFile.close();
-    // auto props = voronoi.analyze();
+    auto props = voronoi.analyze();
+    LOG << fmt::format("actual area = {}", props.area);
     // UT_ASSERT_NEAR(props.area, 4 * M_PI, tol);
 
-    // voronoi.merge();
+    voronoi.merge();
 
     // LOG << fmt::format("writing {} polygons", voronoi.polygons().n());
     // if (voronoi.polygons().n() > 0)
