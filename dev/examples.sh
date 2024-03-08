9d8ad7c34b21a95281e342ba8f8f1440dde5e1c6
Philip Caplan <caplan.philip@gmail.com>
2024-03-08 12:21:33 -0500

    use version 3 for meshb files

--- dev/examples.sh
+++ dev/examples.sh
@@ -4,7 +4,10 @@ vortex=$1
 
 # create adapted triangle mesh of oceans and continents
 $vortex mesh data/oceans_2048.png --hmin 0.02 --output data/earth_coarse.meshb
-$vortex mesh data/oceans_2048.png --hmin $2 --output data/earth.meshb
+if [ ! -f data/earth.meshb ]; then
+  # pass 0.02 in workflow, but 0.005 for better resolution
+  $vortex mesh data/oceans_2048.png --hmin $2 --output data/earth.meshb
+fi
 
 # extract oceans and land
 $vortex extract data/earth.meshb --oceans data/oceans.meshb --continents data/land.meshb
