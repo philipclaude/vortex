c01988b6dc9a45b188aee545d75b20c76a892e27
Philip Caplan <caplan.philip@gmail.com>
2024-03-08 11:33:13 -0500

    missing files in workflow

--- dev/examples.sh
+++ dev/examples.sh
@@ -4,10 +4,7 @@ vortex=$1
 
 # create adapted triangle mesh of oceans and continents
 $vortex mesh data/oceans_2048.png --hmin 0.02 --output data/earth_coarse.meshb
-if [ ! -f data/earth.meshb ]; then
-  # pass 0.02 in workflow, but 0.005 for better resolution
-  $vortex mesh data/oceans_2048.png --hmin $2 --output data/earth.meshb
-fi
+$vortex mesh data/oceans_2048.png --hmin $2 --output data/earth.meshb
 
 # extract oceans and land
 $vortex extract data/earth.meshb --oceans data/oceans.meshb --continents data/land.meshb
