f0797774b87ba8bc8b3ab855d4e13429a0b394ef
ZEYI TONG <96027375+zeyiiiii@users.noreply.github.com>
2024-06-18 12:49:25 -0400

    refactor random point generation for SphereDomain (#47)
    * refactor random point generation for SphereDomain
    
    * fix formatting
    
    * fix a error in build process
    
    * fix formatting
    
    * delete a not used line
    
    * Update src/vortex.cpp
    
    Co-authored-by: Philip Caplan <caplan.philip@gmail.com>
    
    ---------
    
    Co-authored-by: Zeyi Tong <zeyitong@zeyidembp.middlebury.edu>
    Co-authored-by: Philip Caplan <caplan.philip@gmail.com>

--- src/voronoi.h
+++ src/voronoi.h
@@ -26,6 +26,7 @@
 #include "defs.h"
 #include "log.h"
 #include "mesh.h"
+#include "util.h"
 
 namespace vortex {
 
@@ -488,6 +489,12 @@ struct SphereDomain {
   void initialize(vec4 z, VoronoiPolygon<SphereDomain>& cell) const;
   const double radius{1.0};
   double initialization_fraction{0.1};  // use 0.7 for power diagrams
+
+  static vec3d random_point_on_sphere() {
+    coord_t theta = 2.0 * M_PI * irand(0, 1);
+    coord_t phi = acos(2.0 * irand(0, 1) - 1.0);
+    return {cos(theta) * sin(phi), sin(theta) * sin(phi), cos(phi)};
+  }
 };
 
 struct PlanarVoronoiPolygon {
