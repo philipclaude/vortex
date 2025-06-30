f7e52222357c31e7d8a34120f89358cc6ad39cd1
smillay1 <smillay55@gmail.com>
2025-06-30 14:53:51 -0400

    made suggested changes: set dim to 3 for z grid test and changed to LOG

--- src/util_test.cpp
+++ src/util_test.cpp
@@ -18,156 +18,120 @@
 //
 
 #include "util.h"
-#include "tester.h"
-#include <vector>
-#include <limits>
-#include "log.h"
-#include "library.h"
-#include "io.h"
 
+#include <limits>
+#include <vector>
+
+#include "io.h"
+#include "library.h"
+#include "log.h"
+#include "tester.h"
 
 using namespace vortex;
 
 UT_TEST_SUITE(util_test_suite)
 
 UT_TEST_CASE(test_get_bounding_box) {
-    const int dim = 2;
-    const int64_t n_points = 7;
-    std::vector<double> points = {
-        1.0, 5.0,
-        3.0, 7.0,
-        -2.0, 4.0,
-        0.5, -0.5,
-        0.0, 0.0,
-        -0.6, 3.0,
-        0.0,0.0
+  const int dim = 2;
+  const int64_t n_points = 7;
+  std::vector<double> points = {1.0, 5.0, 3.0,  7.0, -2.0, 4.0, 0.5, -0.5,
+                                0.0, 0.0, -0.6, 3.0, 0.0,  0.0
 
-    };
+  };
 
-    double xmin[dim], xmax[dim];
+  double xmin[dim], xmax[dim];
 
-    get_bounding_box(points.data(), n_points, dim, xmin, xmax);
+  get_bounding_box(points.data(), n_points, dim, xmin, xmax);
 
-    UT_ASSERT_EQUALS(xmin[0], -2.0);
-    UT_ASSERT_EQUALS(xmin[1], -0.5);
+  UT_ASSERT_EQUALS(xmin[0], -2.0);
+  UT_ASSERT_EQUALS(xmin[1], -0.5);
 
-    UT_ASSERT_EQUALS(xmax[0], 3.0);
-    UT_ASSERT_EQUALS(xmax[1], 7.0);
+  UT_ASSERT_EQUALS(xmax[0], 3.0);
+  UT_ASSERT_EQUALS(xmax[1], 7.0);
 }
 
 UT_TEST_CASE_END(test_get_bounding_box)
 
-
-
-
-
-
 UT_TEST_CASE(test_sort_points_on_zcurve_one_z) {
-    const int dim = 2;
-    const int64_t n_points = 4;
-    std::vector<double> points = {
+  const int dim = 2;
+  const int64_t n_points = 4;
+  std::vector<double> points = {
 
-        0.0, 0.0,
-        1.0, 0.0,
-        0.0, 1.0,
-        1.0, 1.0, 
+      0.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0, 1.0,
 
-    };
+  };
 
-    std::vector<index_t> order(n_points);
-    sort_points_on_zcurve(points.data(), n_points, dim, order);
+  std::vector<index_t> order(n_points);
+  sort_points_on_zcurve(points.data(), n_points, dim, order);
 
-    Mesh mesh(dim);
-    coord_t x[dim];
+  Mesh mesh(dim);
+  coord_t x[dim];
 
-    for (int i = 0; i < n_points; i++) {
-        int idx = order[i];
-        x[0] = points[dim * idx + 0]; //x
-        x[1] = points[dim * idx + 1]; //y
-        mesh.vertices().add(x);
-    }
+  for (int i = 0; i < n_points; i++) {
+    int idx = order[i];
+    x[0] = points[dim * idx + 0];  // x
+    x[1] = points[dim * idx + 1];  // y
+    mesh.vertices().add(x);
+  }
 
+  for (int i = 1; i < n_points; i++) {
+    index_t line[2] = {static_cast<index_t>(i - 1), static_cast<index_t>(i)};
+    mesh.lines().add(line, 2);
+  }
 
-    for (int i = 1; i < n_points; i++) {
-        index_t line[2] = {static_cast<index_t>(i - 1), static_cast<index_t>(i)};
-        mesh.lines().add(line, 2);
-    }
+  meshb::write(mesh, "zcurve.meshb");
 
-    meshb::write(mesh, "zcurve.meshb");
+  std::vector<index_t> expected_order = {0, 1, 2, 3};
+  UT_ASSERT(order.size() == expected_order.size());
+  for (size_t i = 0; i < order.size(); ++i) {
+    UT_ASSERT(order[i] == expected_order[i]);
+  }
 
-
-    std::vector<index_t> expected_order = { 0, 1, 2, 3};
-    UT_ASSERT(order.size() == expected_order.size());
-    for (size_t i = 0; i < order.size(); ++i) {
-        UT_ASSERT(order[i] == expected_order[i]);
-    }
-
-    //Prints out the actual order of the z-curve
-    std::cout << "Z-Order :\n";
-    for (auto idx : order) {
-        std::cout << idx << " ";
-    }
-    std::cout << std::endl;
-
-
-
-
-
+  // Prints out the actual order of the z-curve
+  LOG << "Z-curve order: ";
+  for (auto idx : order) {
+    std::cout << idx << " ";
+  }
+  std::cout << std::endl;
 }
 
 UT_TEST_CASE_END(test_sort_points_on_zcurve_one_z)
 
-
-
-
-
-
-
-
 UT_TEST_CASE(test_sort_points_on_z_curve_grid) {
-    const int dim = 2;
-    const int nx = 4;
-    const int ny = 4;
+  const int dim = 3;
+  const int nx = 4;
+  const int ny = 4;
 
+  Grid<Quad> grid({nx, ny});
 
-    Grid<Quad> grid({nx, ny});
+  const int64_t n_points = grid.vertices().n();
+  std::vector<double> points(n_points * dim);
 
-    const int64_t n_points = grid.vertices().n();
-    std::vector<double> points(n_points * dim);
+  for (int64_t i = 0; i < n_points; ++i) {
+    const double* v = grid.vertices()[i];
+    points[dim * i + 0] = v[0];
+    points[dim * i + 1] = v[1];
+  }
 
+  std::vector<index_t> order(n_points);
+  sort_points_on_zcurve(points.data(), n_points, dim, order);
 
-    for (int64_t i = 0; i < n_points; ++i) {
-        const double* v = grid.vertices()[i];
-        points[dim * i + 0] = v[0];
-        points[dim * i + 1] = v[1];
-    }
+  Mesh mesh(dim);
+  coord_t x[dim];
+  for (int64_t i = 0; i < n_points; ++i) {
+    int idx = order[i];
+    x[0] = points[dim * idx + 0];  // x
+    x[1] = points[dim * idx + 1];  // y
+    mesh.vertices().add(x);
+  }
 
-    std::vector<index_t> order(n_points);
-    sort_points_on_zcurve(points.data(), n_points, dim, order);
+  for (int i = 1; i < n_points; ++i) {
+    index_t line[2] = {static_cast<index_t>(i - 1), static_cast<index_t>(i)};
+    mesh.lines().add(line, 2);
+  }
 
-
-    Mesh mesh(dim);
-    coord_t x[dim];
-    for (int64_t i = 0; i < n_points; ++i) {
-        int idx = order[i];
-        x[0] = points[dim * idx + 0]; //x
-        x[1] = points[dim * idx + 1]; //y
-        mesh.vertices().add(x);
-    }
-
-    for (int i = 1; i < n_points; ++i) {
-        index_t line[2] = {static_cast<index_t>(i - 1), static_cast<index_t>(i)};
-        mesh.lines().add(line, 2);
-    }
-
-
-
-
-    meshb::write(mesh, "zcurve_grid.meshb");
-
-
+  meshb::write(mesh, "zcurve_grid.meshb");
 }
 UT_TEST_CASE_END(test_sort_points_on_z_curve_grid)
-
 
 UT_TEST_SUITE_END(util_test_suite)
